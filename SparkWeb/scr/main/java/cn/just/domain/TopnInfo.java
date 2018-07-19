af23278eeff2cc280e4ecd5c57e6d192559cfbc0
刘军强 <2671268148@qq.com>
2018-07-19 15:53:51 +0800

    Add files via upload

---
+++ SparkWeb/scr/main/java/cn/just/domain/TopnInfo.java
@@ -0,0 +1,21 @@
+package cn.just.domain;
+
+public class TopnInfo {
+	private String name;
+	private Long value;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getValue() {
+		return value;
+	}
+	public void setValue(Long value) {
+		this.value = value;
+	}
+	
+	
+
+}
